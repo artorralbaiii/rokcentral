5acfc6a9156e22cdaf7bcd8c700399422cd7e544
Andres Jr. III R. Torralba <a.r.torralba.iii@gmail.com>
2017-07-12 11:12:24 +0800

    Create Sign-Up View

---
+++ src/client/app/sign-up/sign-up.controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+    angular.module('app.sign-up')
+        .controller('SignUpController', SignUpController);
+
+    SignUpController.$inject = [];
+
+    function SignUpController() {
+        var vn = this;
+    }
+
+})();
---
+++ src/client/app/sign-up/sign-up.module.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function () {
+    angular.module('app.sign-up', []);
+})();
